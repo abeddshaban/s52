fd7f13402c6060289682bf3a9d7b46a7b1cbaf74
lilXA1 <abedshaaban600@gmail.com>
2022-02-22 20:04:13 +0200

    update

--- pages/Header.js
+++ pages/Header.js
@@ -7,8 +7,8 @@ export default function Header() {
         <span className={globals.header_span}>Abed Al Ghani</span>
         <div className={globals.header_navigation_div}>
           <button className={globals.header_btn}>Home</button>
-          <button className={globals.header_btn}>About</button>
           <button className={globals.header_btn}>Projects</button>
+          <button className={globals.header_btn}>About</button>
         </div>
       </header>
     </>
